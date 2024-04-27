3da92d250b4718b9fbec610b363a4afa52f7e371
kurap1ka00 <145531632+kurap1ka00@users.noreply.github.com>
2024-04-27 09:18:20 +0300

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,12 +1,12 @@
 def find_mm(str):
     bfint=0
     bfstr=''
-    for i in range(1, int(len(str)/2)-1):
+    for i in range(1, len(str)):
         for j in range(0,i):
-            if(str.count(str[j:i])>bfint):
+            if(str[j:i])>=bfint and len(bfstr)<len(str[j:i])):
                 bfint=str.count(str[j:i])
                 bfstr=str[j:i]
-            elif(str.count(str[j:i])==bfint and len(bfstr)<len(str[j:i])):
+            elif(str.count(str[j:i])>bfint):
                 bfint=str.count(str[j:i])
                 bfstr=str[j:i]
     return bfint, bfstr        
